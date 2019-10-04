4c3f9c4118ffc68da29f8e93e95c6ff63bf61a54
EthanShin <avoirom@gmail.com>
2019-10-04 21:43:04 +0900

    feat: Use reduceRightIndexed
    description:
    https://programmers.co.kr/learn/courses/30/lessons/42588
    
    test#1 success
    test#2 success
    test#3 failure
    input = [1, 5, 3, 6, 7, 6, 5]
    expect = [0, 0, 2, 0, 0, 5, 6]
    output = [0, 0, 0, 0, 0, 5, 6]

--- 탑.kt
+++ 탑.kt
@@ -1,6 +1,24 @@
 class Solution {
     fun solution(heights: IntArray): IntArray {
         var answer = intArrayOf()
-        return answer
+        var jump = 0
+
+        heights.reduceRightIndexed { index, i, acc ->
+
+            if (acc >= i) {
+                jump++
+                acc
+            } else {
+                for (i in 0..jump)
+                    answer += index + 1
+                jump = 0
+                i
+            }
+        }
+
+        for (i in 0..jump)
+            answer += 0
+
+        return answer.reversedArray()
     }
 }
